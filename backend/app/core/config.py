55026aeb5cfbdcc3839094243365530a6848446c
ilyassulen <suelen.ilyas@gmail.com>
2026-05-15 15:21:46 +0200

    Setup database foundation and initial user model

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
@@ -10,8 +10,9 @@ class Settings(BaseSettings):
 
     FRONTEND_URL: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file="backend/.env",
+    )
 
 
 settings = Settings()
---
+++ backend/app/models/user.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from backend.app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True,
+    )
+
+    first_name = Column(
+        String,
+        nullable=False,
+    )
+
+    last_name = Column(
+        String,
+        nullable=False,
+    )
+
+    username = Column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+
+    email = Column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+
+    hashed_password = Column(
+        String,
+        nullable=False,
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
